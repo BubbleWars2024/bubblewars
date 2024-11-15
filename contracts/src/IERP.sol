dd7008c8a0a7c9df15b24d58c93be985ac3e35d4
ParanoidSyntaxError <git@jbojtor.com>
2024-11-15 23:30:37 +0700

    Added params to IERPHook, added events and errors to ERP

--- contracts/src/IERP.sol
+++ contracts/src/IERP.sol
@@ -2,6 +2,23 @@
 pragma solidity ^0.8.27;
 
 interface IERP {
+    error ERPReferralIsZeroAddress();
+    error ERPReferralIsSender();
+
+    event NewReferralProgram(address indexed to, uint256 indexed programId);
+    event SetReferral(
+        uint256 indexed programId,
+        address indexed account,
+        address indexed referral
+    );
+
+    struct ReferralProgram {
+        mapping(address => uint256) totalReferrals;
+        mapping(address => address) referrals;
+        address[] beforeHooks;
+        address[] afterHooks;
+    }
+
     function getTotalReferrals(
         uint256 programId,
         address account
--- contracts/src/IERPHook.sol
+++ contracts/src/IERPHook.sol
@@ -2,6 +2,6 @@
 pragma solidity ^0.8.27;
 
 interface IERPHook {
-    function beforeReferral() external;
-    function afterReferral() external;
+    function beforeReferral(uint256 programId, address account, address referral) external;
+    function afterReferral(uint256 programId, address account, address referral) external;
 }
